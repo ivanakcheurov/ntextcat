2202c19d7382e7e7d002d319247aad20b56c8ed1
IvanAkcheurov_cp <SND\IvanAkcheurov_cp>
2012-05-21 14:19:21 -0700

    Adding test data to Test.sql

--- trunk/SqlServerClrIntegration/Test Scripts/Test.sql
+++ trunk/SqlServerClrIntegration/Test Scripts/Test.sql
@@ -33,6 +33,16 @@
 
 --select 'To run your project, please edit the Test.sql file in your project. This file is located in the Test Scripts folder in the Solution Explorer.'
 
-select dbo.IdentifyLanguage('З вами краще не суперечити', N'Wikipedia-MostCommon-Utf8') as idl
+DECLARE @text NVARCHAR(MAX)
+SET @text = N'Вболівальники, які придбали квитки на матчі Євро-2012 о 21:45, матимуть можливість подивитися на стадіоні відразу два матчі, якщо прийдуть на арену заздалегідь.'
+select dbo.IdentifyLanguage(@text, N'Wikipedia-MostCommon-Utf8') as idl
+select dbo.IdentifyLanguageEx(@text, N'Wikipedia-MostCommon-Utf8', 50, 0, 1194329582398, 1.05, 5) as idl
+select * from dbo.IdentifyLanguageTable(@text, N'Wikipedia-MostCommon-Utf8')
+select * from dbo.IdentifyLanguageTableEx(@text, N'Wikipedia-MostCommon-Utf8', 50, 0, 1194329582398, 1.05, 5)
+
+select * from dbo.IdentifyLanguageAndEncodingTableEx(0x48657861646563696d616c206e6f746174696f6e206973207573656420617320612068756d616e2d667269656e646c7920726570726573656e746174696f6e206f662062696e6172792076616c75657320696e20636f6d70757465722070726f6772616d6d696e6720616e64206469676974616c20656c656374726f6e6963732e204d6f73742070726f6772616d6d696e67206c616e6775616765732073756368206173204a6176612c204153502e4e45542c20432b2b2c20466f727472616e206574632068617665206275696c742d696e2066756e6374696f6e73207468617420, N'Wikipedia-MostCommon-Legacy__All-Utf8', 250, 0, 1194329582398, 1.05, 5)
+select * from dbo.IdentifyLanguageAndEncodingTable(CAST(@text AS VARBINARY), N'Wikipedia-MostCommon-Legacy__All-Utf8')
+select * from dbo.IdentifyLanguageAndEncodingTableEx(CAST(@text AS VARBINARY), N'Wikipedia-MostCommon-Legacy__All-Utf8', 50, 0, 1194329582398, 1.05, 5)
+
 
 
