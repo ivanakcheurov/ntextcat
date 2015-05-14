0133f4b3dc3a62860d0c6fefbe8920670b24d42f
ivanakcheurov <ivan.akcheurov@gmail.com>
2015-05-14 18:48:00 +0200

    Removing SqlCltIntegration for a moment until it will get completely fixed

--- src/NTextCat/NuGet.Publish.ps1
+++ src/NTextCat/NuGet.Publish.ps1
@@ -1,2 +1,2 @@
-nuget pack NTextCatLib.csproj -IncludeReferencedProjects -Prop Configuration=Release
+nuget pack NTextCat.csproj -IncludeReferencedProjects -Prop Configuration=Release
 NuGet Push IvanAkcheurov.NTextCat.Lib.0.2.1.1.nupkg
